b14d19f0d88e5ab3f8ce1d574c4335ab340f6991
iboyards <iboyards@gmail.com>
2017-03-19 23:57:00 +0300

    Add pre-commit

--- rubocop_pre_commit_hook.rb
+++ rubocop_pre_commit_hook.rb
@@ -1,17 +1,18 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
-ADDED_OR_MODIFIED = /^\s*(A|AM|M)/.freeze
+ADDED_OR_MODIFIED = /^\s*(A|AM|M)/
 
 changed_files = `git status --porcelain`.split(/\n/)
 unstaged_files = `git ls-files -m`.split(/\n/)
 
 changed_files = changed_files.select { |f| f =~ ADDED_OR_MODIFIED }
-changed_files = changed_files.map { |f| f.split(" ")[1] }
+changed_files = changed_files.map { |f| f.split(' ')[1] }
 
 changed_files -= (unstaged_files - changed_files)
 
-changed_files = changed_files.select { |file_name| File.extname(file_name) == ".rb" }
-changed_files = changed_files.join(" ")
+changed_files = changed_files.select { |file_name| File.extname(file_name) == '.rb' }
+changed_files = changed_files.join(' ')
 
 exit(0) if changed_files.empty?
 
